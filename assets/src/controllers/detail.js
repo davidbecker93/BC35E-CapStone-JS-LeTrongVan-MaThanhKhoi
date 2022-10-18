c467935d59fe48ad299b4626711d47f015ac8a29
davidbecker93 <trongvanle93@gmail.com>
2022-10-18 14:19:57 +0700

    them tinh nang

--- assets/src/controllers/detail.js
+++ assets/src/controllers/detail.js
@@ -11,6 +11,7 @@ function getById() {
         console.log(result.data.content);
         var productDetail = result.data.content;
         renderDetail(productDetail);
+        checkGia(productDetail.price)
     });
 
     promise.catch(function (error) {
@@ -37,16 +38,16 @@ function renderDetail(arrProduct) {
                             <li>${arrProduct.size[3]}</li>
                             <li>${arrProduct.size[4]}</li>
                         </ul>
-                        <h5>${arrProduct.price}$</h5>
+                        <h5 id="price">${arrProduct.price}$</h5>
                         <ul class="soLuong d-flex">
                             <li>
-                                <button>+</button>
+                                <button onclick="addSL(1)">+</button>
                             </li>
                             <li>
-                                <span>1</span>
+                                <span id="soL">1</span>
                             </li>
                             <li>
-                                <button>-</button>
+                                <button onclick="addSL(-1)">-</button>
                             </li>
                         </ul>
                         <button onclick="addBuy()">Add to cart</button>
@@ -100,14 +101,44 @@ function renderCard(arrProduct) {
     }
     document.querySelector('#showProduct').innerHTML = content;
 }
-
+//-------Tinh nang them----------
 var count = 0;
+var SL = 1;
+var giaSP = 0;
 function addBuy() {
-    count++;
-    console.log(count);
+    count += SL;
     document.querySelector("#numBuy").innerHTML = "(" + count + ")";
+    SL = 1;
+    tinhTien(SL);
+    document.querySelector('#soL').innerHTML = SL;
 };
+function addSL(a) {
+    switch (a) {
+        case 1:
+            SL++;
+            tinhTien(SL);
+            break;
 
+        case -1:
+            if (SL > 1) {
+                SL = SL - 1;
+                tinhTien(SL);
+            } else {
+                SL = 1;
+                tinhTien(SL);
+            }
+            break;
+    }
+    document.querySelector('#soL').innerHTML = SL;
+};
+function tinhTien(a) {
+    var gia = giaSP * a;
+    document.querySelector("#price").innerHTML = gia + "$";
+};
+function checkGia(b) {
+    giaSP = b;
+    return giaSP;
+}
 window.onload = function () {
     getById();
     getProduct();
